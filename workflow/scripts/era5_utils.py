b3444b1745cad6ad12713d3cf98cff5002813330
alisonpeard <alison.peard@gmail.com>
2025-04-16 14:16:53 +0100

    tidy era5_utils.py

--- workflow/scripts/era5_utils.py
+++ workflow/scripts/era5_utils.py
@@ -10,28 +10,25 @@ long_names = {
     "t2m": "2m_temperature",
 }
 
-def _unpack(params:xr.Dataset):
+def unpack(params:xr.Dataset):
   """Unpack variables as named dict"""
   return {var: params[var] for var in params.data_vars}
 
-def identity(a):
-    return a
+def identity(x):
+    return x
 
-def wind_speed(a, b):
-    """
-    Compute the l2-norm of two easterly and northerly wind components.
-    """
-    return np.sqrt(a**2 + b**2)
+def wind_speed(u, v):
+    return np.sqrt(u**2 + v**2)
 
-def wind_direction(a, b):
-    """
-    Compute the wind direction in degrees from the u and v components.
-    """
-    return (np.arctan2(a, b) * 180 / np.pi + 360) % 360
+def wind_direction(u, v):
+    return (np.arctan2(u, v) * 180 / np.pi + 360) % 360
 
 # wind power functions
-def wind_power(u10:xr.DataArray, v10:xr.DataArray, params:xr.Dataset):
+def wind_power(u10:xr.DataArray, v10:xr.DataArray, params:xr.Dataset
+               ) -> xr.DataArray:
   """
+  Calculate wind power potential.
+
   Args:
     u10 : xr.DataArray
         10m u component of wind
@@ -42,25 +39,26 @@ def wind_power(u10:xr.DataArray, v10:xr.DataArray, params:xr.Dataset):
         For this function need:
         	- hub scaling parameters (hub_height, roughness)
             - 3PLE parameters (alpha, beta, ws0)
+  
+  References:
+  ..[1] https://doi.org/10.1016/j.rser.2019.04.065
+  ..[2] https://doi.org/10.1016/j.epsr.2017.10.028
   """
   def rescale_winds(ws, hub_height, gamma, h0=10.0, **kwargs):
-    """https://doi.org/10.1016/j.rser.2019.04.065"""
     ws_rescaled = ws * (hub_height / h0)**gamma
     return ws_rescaled
   
   def ple3(ws, ws0, alpha, beta, **kwargs):
-    """3PLE from https://doi.org/10.1016/j.epsr.2017.10.028"""
     power = alpha / (1 + np.exp(-beta * (ws - ws0)))
     return power
   
   ws = wind_speed(u10, v10) # should already be defined
-  param_dict = _unpack(params)
+  param_dict = unpack(params)
   ws_at_hub = rescale_winds(ws, **param_dict)
   potential = ple3(ws_at_hub, **param_dict)
   return potential
 
 
-# photovoltaic potential / solar power functions
 def solar_power(t2m:xr.DataArray, ssrd:xr.DataArray, u10:xr.DataArray,
                 v10:xr.DataArray, params:xr.Dataset) -> xr.DataArray:
   """Calculate solar power potential.
@@ -79,6 +77,8 @@ def solar_power(t2m:xr.DataArray, ssrd:xr.DataArray, u10:xr.DataArray,
         For this function need:
             - solar cell temperature parameters (c_0, c_1, c_2, c_3)
             - performance ratio parameters (g_stc, t_ref, gamma)
+    References:
+    ..[1] https://doi.org/10.1016/j.rser.2019.04.065
     """
   def solar_cell_temp(t2m, sr, ws, c_0, c_1, c_2, c_3, **kwargs):
     return c_0 + c_1*t2m + c_2*sr + c_3*ws
@@ -87,7 +87,7 @@ def solar_power(t2m:xr.DataArray, ssrd:xr.DataArray, u10:xr.DataArray,
     return 1 + gamma * (cell_temp - ref_temp)
 
   ws = wind_speed(u10, v10)
-  param_dict = _unpack(params)
+  param_dict = unpack(params)
   cell_temp = solar_cell_temp(t2m, ssrd, ws, **param_dict)
   pr = performance_ratio(cell_temp, **param_dict)
   potential = pr * (ssrd / params['g_stc'])
