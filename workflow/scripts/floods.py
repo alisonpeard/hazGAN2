cdce0c35d9878db57b5b3ede064b4a880fa2c0af
alisonpeard <alison.peard@gmail.com>
2025-04-23 22:14:50 +0100

    end of day

--- workflow/scripts/floods.py
+++ workflow/scripts/floods.py
@@ -11,6 +11,9 @@ ds
 ds['sro'] = ds['anomaly'].sel(field="sro")
 ds['tp'] = ds['anomaly'].sel(field="tp")
 ds['ws'] = ds['anomaly'].sel(field="ws")
+
+ds["maxsro"] = ds['sro'].sum(dim=["lon", "lat"])
+ds = ds.sortby("maxsro", ascending=False)
 # %%
 fig, axs = plt.subplots(1, 3, figsize=(16, 4))
 
@@ -19,9 +22,10 @@ ds['sro'].isel(time=0).plot(cmap='Blues', ax=axs[1])
 ds['ws'].isel(time=0).plot(cmap='Spectral_r', ax=axs[2])
 # %%
 from shapely.geometry import box
-datadir = "/Users/alison/Documents/DPhil/data/hydrobasins/hybas_as_lev01-12_v1c"
 import geopandas as gpd
-# %%
+
+datadir = "/Users/alison/Documents/DPhil/data/hydrobasins/hybas_as_lev01-12_v1c"
+
 LEVEL = 8
 EXTENT = box(80, 10, 95, 25)
 file = f"hybas_as_lev{str(LEVEL).zfill(2)}_v1c.shp"
@@ -29,55 +33,32 @@ gdf = gpd.read_file(os.path.join(datadir, file))
 gdf = gdf.to_crs("EPSG:4326")
 basins = gdf.clip(EXTENT)
 # %%
-
-import rioxarray
+# import rioxarray
 
 ds.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
 ds.rio.write_crs("epsg:4326", inplace=True)
-
-# %%
 clipped = ds.rio.clip(gdf.geometry, gdf.crs, drop=True)
-# %%
-clipped['sro'].isel(time=0).plot(cmap='Blues')
-# %%
 runoff = clipped['sro'].to_dataframe().reset_index()[['time', 'lon', 'lat', 'sro']]
 runoff = gpd.GeoDataFrame(runoff, geometry=gpd.points_from_xy(runoff.lon, runoff.lat))
 runoff = runoff.set_crs("EPSG:4326")
-# %%
 runoff = runoff.dropna(subset='sro')
-runoff.plot('sro')
-# %%
-gdf.plot("SORT")
-# %%
-intersected = gpd.overlay(basins, runoff, how="")
-# %%
+
+
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 runoff.plot(ax=ax, column='sro', cmap='Blues', legend=True)
 basins.plot(ax=ax, color='none', edgecolor='black')
-# intersected.plot(ax=ax, column='sro', cmap='Blues', legend=True)
 
 # %%
-# %%
 intersection = gpd.sjoin(basins, runoff, how="left", predicate="contains")
-# basins.sindex.valid_query_predicates
-# %%
 intersection = intersection.drop(columns=['index_right', 'lon', 'lat'])
 intersection = intersection[["HYBAS_ID","NEXT_DOWN", "SUB_AREA", "SORT", "geometry", "sro"]].groupby(["HYBAS_ID","NEXT_DOWN", "SUB_AREA", "SORT", "geometry"]).sum()
 intersection = intersection.reset_index()
-# %%
-intersection
-# %%
-intersection = gpd.GeoDataFrame(intersection, geometry=intersection.geometry)
-# %%
-intersection.plot("sro", cmap="Blues")
-# %%
 
+intersection = gpd.GeoDataFrame(intersection, geometry=intersection.geometry)
 
 # %%
 # 1 is most downstream
 intersection = intersection.sort_values(by="SORT", ascending=False)
-
-# %%
 intersection["volume"] = intersection["sro"] * intersection["SUB_AREA"]
 intersection.plot("volume", cmap="Blues", legend=True)
 
@@ -114,6 +95,4 @@ while (len(flow_accumulator) > 0):
 totals.plot(VOI, cmap="Blues", norm=LogNorm(), legend=True, ax=axs[1])
 # same plot but log colorscale
 totals.sort_values(by=VOI, ascending=False).head()
-# %%
-
 # %%
