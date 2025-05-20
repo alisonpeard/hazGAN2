aa0f05aff2cbaf58273a87214ef29056b82c8807
alisonpeard <alison.peard@gmail.com>
2025-05-20 21:11:49 +0000

    fixed param plotting

--- workflow/scripts/plot_parameters.py
+++ workflow/scripts/plot_parameters.py
@@ -4,8 +4,8 @@ import geopandas as gpd
 import xarray as xr
 
 import matplotlib.pyplot as plt
-# import cartopy.crs as ccrs
-# import cartopy.feature as cfeature
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import scipy.stats
 import logging
 
@@ -30,14 +30,18 @@ if __name__ == "__main__":
 
     # load coordinates
     coords = xr.open_dataset(DATA_ALL)
-    coords = coords['grid'].to_dataframe().reset_index()
+    coords = coords[["lat","lon"]].to_dataframe().reset_index()
     coords = gpd.GeoDataFrame(
         coords, geometry=gpd.points_from_xy(coords['lon'], coords['lat'])
         ).set_crs("EPSG:4326")
 
     # load GPD-fitted data   
     df = pd.read_parquet(EVENTS)
-    df = df.merge(coords, on="grid")
+    df["lat"] = df["lat"].astype(float).round(6) # about 11 cm
+    df["lon"] = df["lon"].astype(float).round(6)
+    coords["lat"] = coords["lat"].astype(float).round(6)
+    coords["lon"] = coords["lon"].astype(float).round(6)
+    df = df.merge(coords, on=["lat", "lon"])
     df.columns = [col.replace(".", "_") for col in df.columns]
     gdf = gpd.GeoDataFrame(df, geometry="geometry").set_crs("EPSG:4326")
 
@@ -68,14 +72,15 @@ if __name__ == "__main__":
 
     n = 0
     for var, info in FIELDS.items():
-        fig, axs = plt.subplots(1, 4, figsize=(16, 3), sharex=True, sharey=True,
-        # subplot_kw={'projection': ccrs.PlateCarree()}
+        fig, axs = plt.subplots(1, 4, figsize=(14, 3), sharex=True, sharey=True,
+        subplot_kw={'projection': ccrs.PlateCarree()}
         )
 
         # [left, bottom, width, height]
-        ax4 = fig.add_axes([0.825, 0.1, 0.15, 0.8])
-        plt.tight_layout()
-        plt.subplots_adjust(right=0.8) 
+        # ax4 = fig.add_axes([0.825, 0.1, 0.1, 0.8])
+        ax4 = fig.add_axes([0.675, 0.25, 0.15, 0.55])
+        # ax4 = fig.add_axes([0.8, 0, 0.2, 1])
+        plt.subplots_adjust(right=0.65) 
 
         cmap = CMAP
         p_cmap = plt.get_cmap(cmap)
@@ -89,10 +94,12 @@ if __name__ == "__main__":
         dist = getattr(scipy.stats, scipy_distn)
 
         try:
-            ds[f"pk_{var}"].plot(ax=axs[0], cmap=p_cmap, vmin=PCRIT, cbar_kwargs={'label': None})
-            ds[f"thresh_{var}"].plot(ax=axs[1], cmap=cmap, cbar_kwargs={'label': None})
-            ds[f"scale_{var}"].plot(ax=axs[2], cmap=cmap, cbar_kwargs={'label': None})
-            ds[f"shape_{var}"].plot(ax=axs[3], cmap=cmap, add_colorbar=False) #, vmin=-0.81, vmax=0.28)
+            # make cbars horizontal
+            cbar_kwargs = {"label": None, "shrink": 0.7, "orientation": "horizontal", "pad": 0.05}
+            ds[f"pk_{var}"].plot(ax=axs[0], cmap=p_cmap, vmin=PCRIT, cbar_kwargs=cbar_kwargs)
+            ds[f"thresh_{var}"].plot(ax=axs[1], cmap=cmap, cbar_kwargs=cbar_kwargs)
+            ds[f"scale_{var}"].plot(ax=axs[2], cmap=cmap, cbar_kwargs=cbar_kwargs)
+            ds[f"shape_{var}"].plot(ax=axs[3], cmap=cmap, cbar_kwargs=cbar_kwargs) #, vmin=-0.81, vmax=0.28)
 
             # plot some densities
             shapes_all = gdf[f'shape_{var}'].values
@@ -106,6 +113,7 @@ if __name__ == "__main__":
             norm = plt.Normalize(vmin, vmax)
             colors = [plt.get_cmap(cmap)(norm(value)) for value in shapes]
         
+            # plot the density for three sample grid points
             for i, shape in enumerate(shapes):
                 u = np.linspace(0.95, 0.999, 100)
                 x = dist.ppf(u, shape) #, loc=loc, scale=scale)
@@ -126,14 +134,18 @@ if __name__ == "__main__":
             logging.error(f"Error plotting {var}: {e}")
             continue
 
-        # add colorbar for shape
-        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
-        sm.set_array([])
-        cbar = fig.colorbar(sm, ax=ax4)
-        cbar.set_label(None)
-        cbar.set_label("ξ")
-        cbar.ax.yaxis.set_label_position('left')
-        cbar.ax.set_ylabel('ξ', rotation=0, labelpad=15)
+        # # add colorbar for shape
+        # sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        # sm.set_array([])
+        # cbar = fig.colorbar(sm, ax=ax4, orientation="horizontal", pad=0.05)
+        # cbar.set_label(None)
+        # cbar.set_label("ξ")
+        # cbar.ax.yaxis.set_label_position('left')
+        # cbar.ax.set_ylabel('ξ', rotation=0, labelpad=15)
+
+        # turn off top and right spines for ax4
+        ax4.spines['top'].set_visible(False)
+        ax4.spines['right'].set_visible(False)
 
         # ax4.legend()
         axs[0].set_title("H0: X~{}".format(info["distn"].capitalize()))
@@ -146,5 +158,6 @@ if __name__ == "__main__":
             ax.set_xlabel("Longitude")
             ax.set_ylabel("Latitude")
 
-        fig.savefig(FIGURES[n], bbox_inches='tight', dpi=300)
+        # plt.tight_layout()
+        fig.savefig(FIGURES[n], bbox_inches="tight", dpi=300)
         n += 1
