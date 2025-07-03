f62087a318de167ffd80f374d1af6f2ab8fbb9cb
alisonpeard <alison.peard@gmail.com>
2025-07-03 14:18:06 +0100

    [0,360) --> [-180,180)

--- projects/poweruk/scripts/storms.py
+++ projects/poweruk/scripts/storms.py
@@ -29,5 +29,34 @@ df
 df['cyclone_id'].nunique(), df.index.nunique()
 # %%
 df.to_csv(os.path.join("..", "resources", f"cyclones_{REGION.lower()}.csv"))
+# %% view what the storms look like
+df = pd.read_csv(os.path.join("..", "resources", f"cyclones_{REGION.lower()}.csv"), index_col="date")
+df.index = pd.to_datetime(df.index)
+
+# save these to rows of a text file
+with open(os.path.join("..", "resources", f"cyclones_{REGION.lower()}.txt"), "w") as f:
+    for date in df.index:
+        f.write(f"{date.strftime('%Y-%m-%d')}\n")
+
 # %%
-pd.read_csv(os.path.join("..", "resources", f"cyclones_{REGION.lower()}.csv"), index_col="date").head()
+with open(os.path.join("..", "resources", f"cyclones_{REGION.lower()}.txt"), 'r') as f:
+    SELECT = [line.strip() for line in f if line.strip()]
+
+# %%
+df = df.reset_index().groupby("cyclone_number").apply(
+    lambda subdf: pd.Series({
+        "date": subdf.loc[subdf["wind"].idxmax(), "date"],
+        "wind": subdf["wind"].max()
+    })
+).reset_index().set_index("date")
+df = df.sort_index()
+# %%
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(figsize=(20, 3))
+
+df["wind"].plot(ax=ax)
+# %%
+df.index
+
+# %%
