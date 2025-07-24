a4e9fff35508499a2b34363cf0ddabbc2dbc75e0
alisonpeard <alison.peard@gmail.com>
2025-07-24 10:57:59 +0000

    antecedent buffer days

--- workflow/src/python/datasets/imdaa.py
+++ workflow/src/python/datasets/imdaa.py
@@ -7,23 +7,32 @@ long_names = {
     "APCP_sfc": "prmsl"
 }
 
-def parse_input_pattern(indir, field):
+def get_input_file_pattern(indir, field):
     """
     Parse the input pattern for the given field.
     """
     return os.path.join(indir, long_names[field], '*.nc')
 
 
-def filter_files(files, year):
+def filter_files(files, year, antecendent_buffer_days=None):
     """
     Filter the files for the given year.
     """
-    def year_in_file(f):
+    def year_in_file(f, year):
         fyear = f.split("_")[-1]
         fyear = fyear[:4]
         return str(fyear) == str(year)
+    
+    files = [f for f in files if year_in_file(f, year)]
 
-    return [f for f in files if year_in_file(f)]
+    if antecendent_buffer_days:
+        nyears = 1 + (antecendent_buffer_days // 365)
+        for dyear in range(1, nyears + 1):
+            antecedent_files = [f for f in files if year_in_file(f, year-dyear)]
+            files.extend(antecedent_files)
+    
+    return files
+
 
 def clip_to_bbox(ds, xmin, xmax, ymin, ymax):
     """
