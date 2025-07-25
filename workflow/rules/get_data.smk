9b0f5a90f732cfae1fafaae4c18b7acf5d88d79e
alisonpeard <alison.peard@gmail.com>
2025-07-25 12:33:44 +0000

    get_data script and results figs

--- workflow/rules/get_data.smk
+++ workflow/rules/get_data.smk
@@ -1,18 +1,6 @@
 """
-Get ERA5 data from the SoGE cluster. Run from repository root.
-
-# environment set up
->>> micromamba create -c conda-forge -c bioconda -n snakemake snakemake
->>> conda activate snakemake
->>> >>> conda install -c conda-forge conda=24.7.1
-install snakemake-executor-plugin-slurm
-
-# run rule from project root while on the login node
->>> micromamba activate snakemake
->>> snakemake --profile profiles/slurm/ --executor slurm get_data --use-conda --jobs 20
+Get ERA5 data from the SoGE cluster. Run rules from repository root.
 """
-
-
 def get_param_file():
     """Get a parameter file for the project, or a grid file for the dataset."""
     if os.path.exists(os.path.join(RESOURCES_DIR, "params.nc")):
@@ -41,7 +29,7 @@ rule get_year:
     """
     >>> snakemake --profile profiles/slurm/ --executor slurm --jobs 1 projects/bayofbengal_era5/results/processing/input/2020.nc
     >>> snakemake --profile profiles/cluster --jobs 1 projects/bayofbengal_era5/results/processing/input/2020.nc
-    >>> snakemake --profile profiles/cluster --jobs 1 projects/poweruk/results/processing/input/2020.nc
+    >>> snakemake --profile profiles/cluster --jobs 1 projects/poweruk/results/processing/input/2017.nc
     """
     input:
         indir=INDIR,
--- workflow/src/python/datasets/era5.py
+++ workflow/src/python/datasets/era5.py
@@ -16,27 +16,33 @@ long_names = {
 }
 
 def get_input_file_pattern(indir, field):
-    """
-    Parse the input pattern for the given field.
-    """
+    """Parse the input file pattern for the given field."""
     return os.path.join(indir, long_names[field], 'nc', '*.nc')
 
 
 def filter_files(files, year, antecedent_buffer_days=None):
-    """
-    Filter the files for the given year.
-    """
-    files = [f for f in files if str(year) in f]
+    """Filter the files for the given year."""
+    files_year = [f for f in files if str(year) in f]
+
     if antecedent_buffer_days:
         nyears = 1 + (antecedent_buffer_days // 365)
         for dyear in range(1, nyears + 1):
             antecedent_files = [f for f in files if str(eval(year) - dyear) in f]
-        files.extend(antecedent_files)
-    return files
+        files_year.extend(antecedent_files)
+
+    return files_year
 
 
 def clip_to_bbox(ds, xmin, xmax, ymin, ymax):
-    """
-    Clip the dataset to the given bounding box.
-    """
+    """Clip the dataset to the given bounding box."""
     return ds.sel(longitude=slice(xmin, xmax), latitude=slice(ymax, ymin))
+
+
+def preprocess(ds):
+    """Extra handline to preprocess the dataset."""
+    for coord in ["expver", "number"]:
+        if coord in ds.dims:
+            ds = ds.sel({coord: ds[coord][0]})
+        if coord in ds.coords:
+            ds = ds.reset_coords(coord, drop=True)
+    return ds
