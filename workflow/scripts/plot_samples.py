4ecf0b58f3e91498bbe56bc1778f5ef95ee9801b
alisonpeard <alison.peard@gmail.com>
2025-08-07 08:54:11 +0000

    re-running for longer

--- workflow/scripts/plot_samples.py
+++ workflow/scripts/plot_samples.py
@@ -7,10 +7,27 @@ import ssl
 import numpy as np
 import xarray as xr
 
+from src import funcs
 from src import plotting
 from src import statistics
 
+
 ssl._create_default_https_context = ssl._create_stdlib_context
+
+
+def rank_by_intensity_func(ds:xr.Dataset, subset:dict):
+    """Rank the dataset using config-defined function and threshold."""
+    func = getattr(funcs, subset["func"])
+    args = subset["args"]
+    thresh = subset["value"]
+
+    for arg in args:
+        ds[arg] = ds.sel(field=arg).anomaly
+
+    intensity = func(ds, *args, dim=["lon", "lat"])
+    ranking = np.argsort(intensity)[::-1]
+
+    return ranking
 
 
 def main(input, output, params):
@@ -24,21 +41,26 @@ def main(input, output, params):
     train_x = train["anomaly"].values
     train_u = train["uniform"].values
     train_y = transform(train_u)
-    # train_y = train["standardised"].values # only for comparison training set
+    # train_y = train["standardised"].values # if using comparison data instead
 
     gener = xr.open_dataset(input.generated)
     gener_x = gener["anomaly"].values
     gener_u = gener["uniform"].values
-    # gener_y = transform(gener_u)
+    # gener_y = transform(gener_u) # if using comparison data instead
     gener_y = gener["standardised"].values
 
     if params.shuffle:
         train_ids = np.random.permutation(train_u.shape[0])
         gener_ids = np.random.permutation(gener_u.shape[0])
     else:
-        # NOTE: sorting by var 1, make more flexible
-        train_ids = np.argsort(np.max(train_u[..., 0], axis=(1, 2)))[::-1]
-        gener_ids = np.argsort(np.max(gener_u[..., 0], axis=(1, 2)))[::-1]
+        # sort by intensity fn
+        if params["intensity"]["do"]:
+            train_ids = rank_by_intensity_func(train, params.intensity)
+            gener_ids = rank_by_intensity_func(gener, params.intensity)
+        else:
+            # just sort by first field
+            train_ids = np.argsort(np.max(train_u[..., 0], axis=(1, 2)))[::-1]
+            gener_ids = np.argsort(np.max(gener_u[..., 0], axis=(1, 2)))[::-1]
 
     xmin = params["lon_min"]
     xmax = params["lon_max"]
@@ -50,7 +72,7 @@ def main(input, output, params):
     for i, field in enumerate(params.fields.keys()):
         metric = params.fields[field].get("units", "")
         cmap   = params.fields[field].get("cmap", "viridis")
-        cmap = plotting.eval_cmap_str(cmap)
+        cmap   = plotting.eval_cmap_str(cmap)
 
         figa = plotting.samples.plot(
             gener_y[gener_ids], train_y[train_ids], field=i, title="",
