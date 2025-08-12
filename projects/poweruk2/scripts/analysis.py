8a65a2830c518dabc2484c88aa692d3a788b913a
alisonpeard <alison.peard@gmail.com>
2025-08-12 07:33:54 +0000

    checkpoint:think I fixed medians bug

--- projects/poweruk2/scripts/analysis.py
+++ projects/poweruk2/scripts/analysis.py
@@ -41,15 +41,18 @@ bbox = gdf_subset.total_bounds
 train = xr.open_dataset(os.path.join("..", "results", "training", "data.nc"))
 gener = xr.open_dataset(os.path.join("..", "results", "generated", "netcdf", "data.nc"))
 
-train["l2norm"] = np.sqrt(train.sel(field="u10").anomaly**2 + train.sel(field="v10").anomaly**2)
-mask = train.l2norm.max(dim=["lon", "lat"]) > 25
-train = train.isel(time=mask)
+# train["l2norm"] = np.sqrt(train.sel(field="u10").anomaly**2 + train.sel(field="v10").anomaly**2)
+# mask = train.l2norm.max(dim=["lon", "lat"]) > 25
+# train = train.isel(time=mask)
 # %%
 medians = train.medians.sel(month="December") # this should only have months for season
 
 # %%
-# data = train.copy() + medians
-data = gener.copy() + medians
+dataset = ["train", "generated"][0]
+if dataset == "train":
+    data = train.copy() + medians
+else:
+    data = gener.copy() + medians
 
 data["u10"] = data.sel(field="u10").anomaly
 data["v10"] = data.sel(field="v10").anomaly
@@ -73,23 +76,23 @@ ax.bar(dirs, speeds, normed=True, opening=0.8, edgecolor='black',
         cmap=cmo.speed, linewidth=0.5)
 ax.set_legend()
 # %%
-t = np.random.choice(range(data.sizes["time"]), 1)[0]
+t = int(np.random.choice(range(data.sizes["time"]), 1)[0])
 
 print(f"Selected time step: {t}")
 
-data = data.isel(time=t)
+data_t = data.isel(time=t)
 
-resample = data.isel(lon=slice(None, None, 7), lat=slice(None, None, 7))
+resample = data_t.isel(lon=slice(None, None, 7), lat=slice(None, None, 7))
 
 fig, axs = plt.subplots(1, 2, figsize=(18, 10), subplot_kw={"projection": ccrs.PlateCarree()})
 
 
 ax = axs[0]
-data.vx.plot(
+data_t.vx.plot(
     ax=ax, cmap=cmo.speed,
     cbar_kwargs={"label": "max gust speed (m/s)", "shrink": 0.6}
     )
-data.plot.streamplot(
+data_t.plot.streamplot(
     x='lon', y='lat', u='u10', v='v10', 
     transform=ccrs.PlateCarree(), color="white", ax=ax, density=1.5,
     linewidth=0.5, arrowstyle='->', arrowsize=2
@@ -103,7 +106,7 @@ ax.set_ylim(bbox[1], bbox[3])
 ax.set_title("Wind speed and direction")
 
 ax = axs[1]
-data.r30.plot(
+data_t.r30.plot(
     ax=ax, cmap=cmo.rain,
     cbar_kwargs={"label": "30-day rainfall (m)", "shrink": 0.6}
     )
@@ -113,5 +116,24 @@ ax.set_xlim(bbox[0], bbox[2])
 ax.set_ylim(bbox[1], bbox[3])
 ax.set_title("30-day antecedent rainfall")
 
+fig.suptitle(f"{dataset.capitalize()} wind and rainfall for {t=}", fontsize=16, y=0.85)
+
 plt.tight_layout()
 # %%
+"""
+def calculate_nimgs(wildcards, years_of_samples=config["nyears"]):
+    with zipfile.ZipFile(os.path.join(TRAINING_DIR, "images.zip"), 'r') as zip_ref:
+        img_files = [f for f in zip_ref.namelist() if f.lower().endswith(('.png', '.jpg', '.jpeg', '.npy'))]
+        nimgs = len(img_files)
+    nyears = YEARN - YEAR0
+    freq   = nimgs / nyears
+    nsamples = int(freq * years_of_samples)
+    print(f"Calculated number of samples: {nsamples} based on {nimgs} images over {nyears} years.")
+    return nsamples
+
+years_of_samples = 50
+nyears = 2018 - 2004 + 1 = 15
+nimgs = 429
+freq = 429 / 15 = 28.6
+nsamples = 28.6 * 50 = 1430
+"""
