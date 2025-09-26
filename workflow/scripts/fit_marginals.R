cded4449c527c648f8be7a3295430556f734b153
alisonpeard <alison.peard@gmail.com>
2025-09-26 11:24:23 +0000

    think all data processing bugs fixed for poweruk_winter now

--- workflow/scripts/fit_marginals.R
+++ workflow/scripts/fit_marginals.R
@@ -26,7 +26,6 @@ METADATA <- snakemake@input[["metadata"]]
 DAILY    <- snakemake@input[["daily"]]
 OUTPUT   <- snakemake@output[["events"]]
 FIELDS   <- snakemake@params[["fields"]]
-Q        <- snakemake@params[["q"]]
 
 # load input data
 daily    <- read_parquet(DAILY)
@@ -41,57 +40,50 @@ hfunc_args  <- sapply(FIELDS, function(x) x$hfunc$args)
 nfields <- length(fields)
 
 
-field_summary <- function(i) {
+field_summary_msg <- function(i) {
   paste0("Fitting field: ", fields[i], "\n")
 }
 
-# figure out expected output length: metadata
-num_events <- length(unique(metadata$event))
-num_x <- length(unique(daily$lon))
-num_y <- length(unique(daily$lat))
-num_fields <- length(fields)
-expected_rows <- num_events * num_x * num_y * num_fields
-
 # main: fit marginals and transform each field
-log_info("Transforming fields...")
+log_info("\nTransforming fields...")
 
-log_debug(field_summary(1))
+log_debug(field_summary_msg(1))
 events_field1 <- marginal_transformer(
-  daily, metadata, fields[1], Q,
+  daily, metadata, fields[1],
   hfunc = hfuncs[[1]], hfunc_args = hfunc_args[[1]],
   distn = distns[[1]], two_tailed = two_tailed[[1]],
   log_file = log_file, log_level = log_level
 )
 log_info(paste0("Finished fitting: ", fields[1]))
 
-log_debug(field_summary(2))
+log_debug(field_summary_msg(2))
 events_field2 <- marginal_transformer(
-  daily, metadata, fields[2], Q,
+  daily, metadata, fields[2],
   hfunc = hfuncs[[2]], hfunc_args = hfunc_args[[2]],
   distn = distns[[2]], two_tailed = two_tailed[[2]],
   log_file = log_file, log_level = log_level
 )
 log_info(paste0("Finished fitting: ", fields[2]))
 
-log_debug(field_summary(3))
+log_debug(field_summary_msg(3))
 events_field3 <- marginal_transformer(
-  daily, metadata, fields[3], Q,
+  daily, metadata, fields[3],
   hfunc = hfuncs[[3]], hfunc_args = hfunc_args[[3]],
   distn = distns[[3]], two_tailed = two_tailed[[3]],
   log_file = log_file, log_level = log_level
 )
 log_info(paste0("fit_marginals.R - Finished fitting: ", fields[3]))
+log_success("fit_marginals.R - Done. Putting it all together...")
 
 # combine fields
 renamer <- function(df, var) {
   df <- df |>
-    rename_with(~ paste0(., ".", var),
-                -c("lat", "lon", "event", "event.rp", "variable"))
+    rename_with(
+      ~ paste0(., ".", var), -c("lat", "lon", "event", "event.rp", "variable"
+    ))
   df <- df |> rename_with(~ var, "variable")
   return(df)
 }
-
-log_success("fit_marginals.R - Done. Putting it all together...")
 
 events_field1 <- renamer(events_field1, fields[1])
 events_field2 <- renamer(events_field2, fields[2])
@@ -100,8 +92,6 @@ events_field3 <- renamer(events_field3, fields[3])
 events <- events_field1 |>
   inner_join(events_field2, by = c("lat", "lon", "event", "event.rp")) |>
   inner_join(events_field3, by = c("lat", "lon", "event", "event.rp"))
-
-events$thresh.q <- Q # keep track of threshold used
 
 log_debug(paste0(
   "Combined events data has ", nrow(events), " rows."
