573ecf827fb2cd52d19006844d611c5cf1ca9cca
alisonpeard <alison.peard@gmail.com>
2025-09-30 12:36:47 +0000

    fixed scatterplot code (poweruk)

--- workflow/scripts/plot_scatterplots.py
+++ workflow/scripts/plot_scatterplots.py
@@ -18,10 +18,16 @@ def op2idx(ops:dict, data:np.ndarray, extent:list):
     lons = np.linspace(extent[0], extent[1], w)
     lats = np.linspace(extent[2], extent[3], h)
 
-    coords = np.array([Point(lon, lat) for lon in lons for lat in lats])
+    coords = np.array([Point(lon, lat) for lat in lats for lon in lons])
+
+    print(f"{coords=}")
 
     op_idx = {}
     for op, loc in ops.items():
+
+        if isinstance(loc, dict):
+            loc = (loc["lon"], loc["lat"])
+        
         idx = np.argmin([coord.distance(Point(loc)) for coord in coords])
         op_idx[op] = idx
 
@@ -55,9 +61,9 @@ def main(input, output, params):
 
     # load data and samples
     train = xr.open_dataset(input.train)
-    if params["subset"]["do"]:
+    if params["event_subset"]["do"]:
         # subset train by threshold
-        train = subset_func(train, params["subset"])
+        train = subset_func(train, params["event_subset"])
         logging.info(f"\nExtracted {train.time.size} images from train.")
     
     train_x = train["anomaly"].values
@@ -73,13 +79,16 @@ def main(input, output, params):
         params.pois, train_x[0, ..., 0],
         extent=[params.xmin, params.xmax, params.ymin, params.ymax]
         )
-    pixels = [ops["chittagong"], ops["dhaka"]]
+    # pixels = [ops["chittagong"], ops["dhaka"]]
+    pixels = list(ops.values())[:2]
+    labels = list(ops.keys())[:2]
 
     _fields = params.channel_labels.keys()
 
     for i, _field in enumerate(_fields):
         fig = plotting.scatter.plot(gener_x, train_x, field=i, pixels=pixels, s=10,
-                        cmap=params.cmap, xlabel="Chittagong", ylabel="Dhaka")
+                        cmap=params.cmap, xlabel=labels[0].capitalize(), 
+                        ylabel=labels[1].capitalize())
 
         fig.suptitle(params.channel_labels[_field].capitalize(), y=1.05, fontsize=14, fontweight='bold')
         fig.savefig(os.path.join(output.outdir, f"field_{_field}.png"), dpi=300)
