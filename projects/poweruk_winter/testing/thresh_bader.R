4e6433e5989bd41ca686fafda8a4b36f33096966
alisonpeard <alison.peard@gmail.com>
2025-10-16 09:00:18 +0000

    Making tests for marginal fits

---
+++ projects/poweruk_winter/testing/thresh_bader.R
@@ -0,0 +1,9 @@
+# %%
+# install.packages("eva")
+library(eva)
+ 
+# %%
+print("Hello World!")
+print("Now you need to load the PoI netCDFs, and try eva on them. Then you need to compare the results with the results from fit_marginals.R")
+
+# %%
