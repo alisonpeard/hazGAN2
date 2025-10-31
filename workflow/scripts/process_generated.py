2e2f18e36fa3d562225a22b9d002151efab0432b
alisonpeard <alison.peard@gmail.com>
2025-10-31 14:33:41 +0000

    Refactor process_generated.py for modularity

--- workflow/scripts/process_generated.py
+++ workflow/scripts/process_generated.py
@@ -19,101 +19,70 @@ def subset_func(ds:xr.Dataset, subset:dict):
     func = getattr(funcs, subset["func"])
     args = subset["args"]
     thresh = subset["value"]
-
     logging.info(f"Subsetting {func}{*args,} with threshold {thresh}.")
-
     for arg in args:
         ds[arg] = ds.sel(field=arg).anomaly
-
     intensity = func(ds, *args, dim=["lon", "lat"])
-
     for arg in args:
         ds = ds.drop_vars(arg)
-
     mask = (intensity > thresh).values
     idx = np.where(mask)[0]
     return ds.isel(time=idx)
 
 
-def main(input, output, params):
-    # load all images from zipfile
-    with zipfile.ZipFile(input.image_dir, 'r') as zip_ref:
-        flist = [f for f in zip_ref.namelist() if f.endswith('.npy')]
-        logging.info(f"Found {len(flist)} images in {input.image_dir}.")
-        images = []
-        for f in sorted(flist):
-            with zip_ref.open(f) as file:
-                img = np.load(file)
-                images.append(img / 255)
-    images = np.stack(images, axis=0)
-    logging.info(f"Created generated images ndarray of shape {images.shape}.")
-
-    # load training images from zipfile
-    with zipfile.ZipFile(input.training_dir, 'r') as zip_ref:
+def load_npy_from_zip(path:str, K=255) -> np.array:
+    with zipfile.ZipFile(path, 'r') as zip_ref:
         flist = [f for f in zip_ref.namelist() if f.endswith('.npy')]
         logging.info(f"Found {len(flist)} training images in {input.training_dir}.")
-        images_train = []
+        samples = []
         for f in sorted(flist):
             with zip_ref.open(f) as file:
-                img = np.load(file)
-                images_train.append(img / 255)
-    images_train = np.stack(images_train, axis=0)
-    logging.info(f"Created training images ndarray of shape {images_train.shape}.")
+                sample = np.load(file)
+                samples.append(sample / K)
+    samples = np.stack(samples, axis=0)
+    return samples
 
-    # apply image statistics to rescale
-    image_stats = np.load(input.image_stats)
+
+def transform_to_scale(y:np.array, image_stats:dict) -> np.array:
+    """Rescale samples using image statistics."""
     image_minima = image_stats['min']
     image_maxima = image_stats['max']
     image_range  = image_maxima - image_minima
     logging.info(f"Image statistics: min {image_minima}, max {image_maxima}")
+    y_rescaled = y * image_range + image_minima
+    y_rescaled = np.flip(y_rescaled, axis=1)
+    return y_rescaled
 
-    inv_transform = getattr(stats, f"inv_{params.domain}")
 
-    # rescale images to marginals scale
-    images_y = images * image_range + image_minima
-    images_u = inv_transform(images_y)
-    images_y = np.flip(images_y, axis=1)
-    images_u = np.flip(images_u, axis=1)
+def transform_to_uniform(y:np.array, domain:str) -> np.array:
+    inv_transform = getattr(stats, f"inv_{domain}")
+    u = inv_transform(y)
+    return u
 
-    # rescale train in same way
-    compare_y = images_train * image_range + image_minima
-    compare_u = inv_transform(compare_y)
-    compare_y = np.flip(compare_y, axis=1)
-    compare_u = np.flip(compare_u, axis=1)
 
-    # load training netCDF
-    train = xr.open_dataset(input.training_data)
-    
-    if params["subset"]["do"]:
-        # subset train by threshold
-        train = subset_func(train, params["subset"])
-        logging.info(f"\nExtracted {train.time.size} images from train.")
-        
-    train_x = train["anomaly"].values
-    theta  = train["params"].values
-
-    logging.info(f"Loaded anomaly training data of shape {train_x.shape}.")
-    logging.info(f"Loaded parameters of shape {theta.shape}.")
-
-    # check range of uniform samples
-    epsilon = 1e-6
-    if not ((compare_u.max() < 1.) and (compare_u.min() > 0.)):
-        logging.error("Training uniform samples not in [0,1] range")
-        logging.error(f"Max: {compare_u.max()}, Min: {compare_u.min()}")
-        compare_u = np.clip(compare_u, epsilon, 1 - epsilon)
-    
-    if not ((images_u.max() < 1.) and (images_u.min() > 0.)):
+def check_range(u:np.array, epsilon=1e-6):
+    if not ((u.max() < 1.) and (u.min() > 0.)):
         logging.error("Generated uniform samples not in [0,1] range")
-        logging.error(f"Max: {images_u.max()}, Min: {images_u.min()}")
-        images_u = np.clip(images_u, epsilon, 1 - epsilon)
+        logging.error(f"Max: {u.max()}, Min: {u.min()}")
+        u = np.clip(u, epsilon, 1 - epsilon)
+    return u
 
-    # transform images to original scale using invPIT
-    distns = [field['distn'] for field in params.fields.values()]
-    two_tailed = [field['two_tailed'] for field in params.fields.values()]
-    images_x = stats.invPIT(images_u, train_x, theta=theta, distns=distns, two_tailed=two_tailed)
-    compare_x = stats.invPIT(compare_u, train_x, theta=theta, distns=distns, two_tailed=two_tailed)
+def transform_to_physical(u:np.array, ref:xr.Dataset, subset:dict, fields:dict) -> np.array:
+    u = check_range(u)
+    if subset["do"]:
+        ref = subset_func(ref, subset)
+        logging.info(f"\nExtracted {ref.time.size} images from train.")
+    x_ref = ref["anomaly"].values
+    theta  = ref["params"].values
+    logging.info(f"Loaded anomaly training data of shape {x_ref.shape}.")
+    logging.info(f"Loaded parameters of shape {theta.shape}.")
+    distns = [field['distn'] for field in fields.values()]
+    two_tailed = [field['two_tailed'] for field in fields.values()]
+    x = stats.invPIT(u, x_ref, theta=theta, distns=distns, two_tailed=two_tailed)
+    return x
 
-    # save to NetCDF
+
+def construct_dataset(images_x, images_y, images_u, theta, lats, lons, fields):
     data = xr.Dataset(
         {
             "anomaly": (("time", "lat", "lon", "field"), images_x),
@@ -126,39 +95,43 @@ def main(input, output, params):
                 "loc_upper", "scale_upper", "shape_upper",
                 "loc_lower", "scale_lower", "shape_lower"
                 ],
-            "field": list(params.fields.keys()),
+            "field": fields,
             "time": (("time"), np.arange(images_x.shape[0])),
-            "lat": (("lat"), train["lat"].values),
-            "lon": (("lon"), train["lon"].values),
+            "lat": (("lat"), lats),
+            "lon": (("lon"), lons),
         },
     )
+    return data
 
-    data.to_netcdf(output.netcdf, format="NETCDF4")
+
+def main(input, output, params):
+    # load reference data
+    image_stats = np.load(input.image_stats)
+    ref = xr.open_dataset(input.training_data)
+    theta = ref["params"].values
+    lats = ref["lat"].values
+    lons = ref["lon"].values
+
+    # load generated images from zipfile
+    y = load_npy_from_zip(input.image_dir)
+    logging.info(f"Created generated images ndarray of shape {y.shape}.")
+    y = transform_to_scale(y, image_stats)
+    u = transform_to_uniform(y, params.domain)
+    x = transform_to_physical(u, ref, params.subset, params.fields)
+    ds = construct_dataset(x, y, u, theta, lats, lons, list(params.fields.keys()))
+    ds.to_netcdf(output.netcdf, format="NETCDF4")
     logging.info(f"Saved generated data to {output.netcdf}.")
-    logging.info("Done.")
 
-    # save the comparison dataset
-    compare_data = xr.Dataset(
-        {
-            "anomaly": (("time", "lat", "lon", "field"), compare_x),
-            "standardised": (("time", "lat", "lon", "field"), compare_y),
-            "uniform": (("time", "lat", "lon", "field"), compare_u),
-            "params": (("lat", "lon", "param", "field"), theta),
-        },
-        coords={
-            "param": [
-                "loc_upper", "scale_upper", "shape_upper",
-                "loc_lower", "scale_lower", "shape_lower"
-                ],
-            "field": list(params.fields.keys()),
-            "time": (("time"), np.arange(compare_y.shape[0])),
-            "lat": (("lat"), train["lat"].values),
-            "lon": (("lon"), train["lon"].values),
-        },
-    )
-
-    compare_data.to_netcdf(output.train, format="NETCDF4")
+    # repeat for comparison data
+    y = load_npy_from_zip(input.training_dir)
+    y = transform_to_scale(y, image_stats)
+    u = transform_to_uniform(y, params.domain)
+    x = transform_to_physical(u, ref, params.subset, params.fields)
+    ds = construct_dataset(x, y, u, theta, lats, lons, list(params.fields.keys()))
+    ds.to_netcdf(output.train, format="NETCDF4")
     logging.info(f"Saved training data to {output.train}.")
+
+    logging.info("Done.")
 
 
 if __name__     == "__main__":
