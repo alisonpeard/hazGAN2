1a8e83305c34b1582da31bc3de03d85b7f6d94fd
alisonpeard <alison.peard@gmail.com>
2025-11-05 18:49:44 +0000

    Fixed double-transpose bug in image generation

--- workflow/src/stylegan/generate.py
+++ workflow/src/stylegan/generate.py
@@ -118,8 +118,6 @@ def generate_images(
         
         z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
-
-        img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255)# .to(torch.uint8)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128)
 
         np.save(f'{outdir}/seed{seed:04d}.npy', img[0].cpu().numpy())
