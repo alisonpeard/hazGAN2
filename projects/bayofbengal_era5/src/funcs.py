3b2d286d6f2ff13e3d005c0ab9abad07609d7c65
alisonpeard <alison.peard@gmail.com>
2025-12-21 21:56:44 +0000

    Trying to resolve grib stuff

--- projects/bayofbengal_era5/src/funcs.py
+++ projects/bayofbengal_era5/src/funcs.py
@@ -5,8 +5,11 @@ import numpy as np
 import xarray as xr
 
 
-def wind_speed(u:xr.DataArray, v:xr.DataArray, *args, **kwargs) -> xr.DataArray:
-    return np.sqrt(u**2 + v**2)
+def wind_speed(ds:xr.Dataset, u_arg:str, v_arg:str,
+               params={}, dim="time") -> xr.DataArray:
+    u = ds[u_arg]
+    v = ds[v_arg]
+    return np.sqrt(u**2 + v**2).max(dim=dim)
 
 
 def wind_direction(u:xr.DataArray, v:xr.DataArray, *args, **kwargs) -> xr.DataArray:
